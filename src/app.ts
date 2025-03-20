178c83d4c8e4e67f7dd6037093914ec9e379f14a
le cong hien <leconghienlch2006@gmail.com>
2025-03-20 09:55:10 +0700

    video 27

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,18 @@
 import express from "express";
-
+import "dotenv/config";
+import { log } from "node:console";
 const app = express();
-const port = 8080;
+const port = process.env.PORT;
 
 app.get("/", (req, res) => {
-    res.send("le cong hien tro lai roi day");
+    res.send("le cong hien tro lai roi day nodemon");
 });
 
 app.get("/hienle", (req, res) => {
-    res.send("le cong hien ");
+    res.send("le cong hien xin chao ");
 });
 
 app.listen(port, () => {
     console.log(`my app is running on port: ${port}`);
+    console.log(`PORT`, port);
 });
